9837e2e87ea1ba7f1c193a28b3af280508dec3d6
demisu-s <damisusefu@gmail.com>
2024-07-02 10:08:28 +0300

    updated

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -10,7 +10,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
   return (
-    <Container fluid className="home-about-section" id="about">
+    <Container fluid className="home-about-section " id="about">
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/le.jpg";
+
 
 import { Link } from "react-router-dom";
 import {
@@ -36,7 +36,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand  className="d-flex">
-          {/* <img src={logo} className="img-fluid logo " alt="brand" /> */}
+         
          
           
           <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>DS</Link>
